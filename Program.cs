a0ec19652fd04db8bd1086470233f06bd1fcb7eb
Gustavo Lupim <thelinkergu@gmail.com>
2024-04-09 23:12:09 -0300

    3 atividades

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 - Escreva um programa para receber 3 números inteiros e a seguir calcular e exibir qual deles é o maior
+// Encontre o maior dentre 3 números:
+// Primeiro Número : 65465
+// Segundo Número : 64658
+// Terceiro Número : 65464
+// O primeiro número : 65465 é o maior
+
+Console.WriteLine("Digite o seu primeiro número");
+int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+
+// 2- Escreva um programa para exibir os 10 primeiros números naturais e calcular a sua soma usando os loop 
+// while, do-while e for.
+// Os 10 primeiros números naturais são :
+// 1 2 3 4 5 6 7 8 9 10
+// A soma dos números é : 55
+
+// 3- Escreva um programa para exibir a tabela de multiplicação de um número natural maior que zero 
+// recebido via teclado 
+// - Verifique se o número é maior que zero e emita uma mensagem
+// - Considere a tabela de multiplicação de 1 até 10
+// - Após exibir a tabela torne a solicitar outro número 
+// - Para sair do loop defina uma condição de saída 
+// Dica: Use os loop while e for e para sair a instrução break;
+
+// 4- Crie um programa para exbir no console os números pares de 10 a 20, ambos incluídos, exceto 16, de 3 
+// maneiras diferentes:
+// - Incrementando 2 em cada passo
+// - Incrementando 1 em cada passo 
+// - Com e loop infinito (use "break" e "continue")
+
+ 
+
+// 5 - Escreva um programa para calcular o fatorial de um número inteiro. 
+// O fatorial de um número é representado por : n! => n * (n – 1) * (n – 2) ....2*1 
+// Exemplo : fatorial de 6 é representado por 6! = 6*5*4*3*2*1
